f1a88972451d39e910150e7101e30e781e806cdc
zzh <zzh20001022@163.com>
2024-01-15 23:07:18 +0800

    解决分布式锁误删的问题

--- src/main/java/com/hmdp/utils/RedisLock.java
+++ src/main/java/com/hmdp/utils/RedisLock.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -13,19 +14,24 @@ public class RedisLock implements ILock {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    private String uuid;
+
     @Override
     public boolean tryLock(String key, Long TTL) {
         // 获取当前线程标识
-        long id = Thread.currentThread().getId();
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, String.valueOf(id), TTL, TimeUnit.SECONDS);
+        uuid = UUID.randomUUID().toString() + Thread.currentThread().getId();
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, uuid, TTL, TimeUnit.SECONDS);
         // 这里使用工具类防止自动拆包的时候出现空指针异常
         return BooleanUtil.isTrue(flag);
     }
 
     @Override
     public boolean unLock(String key) {
-        Boolean isDelete = stringRedisTemplate.delete(key);
-        return BooleanUtil.isTrue(isDelete);
+        String value = stringRedisTemplate.opsForValue().get(key);
+        if (value == null || value.equals(uuid)) {
+            return false;
+        }
+        return BooleanUtil.isTrue(stringRedisTemplate.delete(key));
     }
 
 }
